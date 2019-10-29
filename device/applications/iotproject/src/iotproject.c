a25a1644e936c96d4b45711434c620b8e2209987
machaj45 <machaj45@fel.cvut.cz>
2019-10-29 16:22:37 +0100

    remove build

--- device/applications/iotproject/src/iotproject.c
+++ device/applications/iotproject/src/iotproject.c
@@ -6,10 +6,10 @@ int main(void) {
   Initialize_OS();
   Initialize_Sensors();
   IWDG_feed(NULL);
-  GSP30_setI2CInterface(&P1_I2C);
+  GSP30_setI2CInterface(&P3_I2C);
   HAL_Delay(1000);
   GSP30_init();
-
+  HAL_Delay(1000);
   LorawanInit();
   osKernelStart();
   while (1) {
