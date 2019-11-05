9a566111dfebad14b793254c06e2af4e0aed5aa3
Student <student@student-02.idlab.uantwerpen.be>
2019-11-05 20:38:01 +0100

    mqtt_subscribe.py works with all includes

--- backend/mqtt_subscriber.py
+++ backend/mqtt_subscriber.py
@@ -2,12 +2,21 @@ import paho.mqtt.client as mqtt # import the client1
 import time
 import argparse
 from bitstring import ConstBitStream
-from d7a.alp.parser import Parser #as AlpParser
-#from d7a.dll.parser import Parser as DllFrameParser, FrameType
-#rom d7a.serial_modem_interface.parser import Parser as SerialParser
-#rom d7a.system_files.system_file_ids import SystemFileIds
-#rom d7a.system_files.system_files import SystemFiles
+from d7a.alp.parser import Parser as AlpParser
+from d7a.dll.parser import Parser as DllFrameParser, FrameType
+from d7a.serial_modem_interface.parser import Parser as SerialParser
+from d7a.system_files.system_file_ids import SystemFileIds
+from d7a.system_files.system_files import SystemFiles
 
+def subscribe_to_our_devices(client):
+        # This is device of
+        client.subscribe("/d7/483638370025002f/#")
+        # This is device of
+        client.subscribe("/d7/483638370041003f/#")
+        # This is device of
+        client.subscribe("/d7/4836383700440045/#")
+        # This is device of
+        client.subscribe("/d7/48363837002a0038/#")
 
 def on_message(client, userdata, message):
         print("message received ", str(message.payload.decode("utf-8")))
@@ -22,6 +31,7 @@ broker_address = "student-04.idlab.uantwerpen.be"
 print("creating new instance")
 client = mqtt.Client("P1") # create new instance 
 client.on_message=on_message # attach function to callback
+subscribe_to_our_devices(client)
 print("connecting to broker")
 client.connect(broker_address) # connect broker
 client.loop_start() # start the loop
@@ -32,12 +42,3 @@ time.sleep(1000) # wait
 client.loop_stop() # stop the loop
 
 
-def subscribe_to_our_devices():
-        # This is device of
-        client.subscribe("/d7/483638370025002f/#")
-        # This is device of
-        client.subscribe("/d7/483638370041003f/#")
-        # This is device of
-        client.subscribe("/d7/4836383700440045/#")
-        # This is device of
-        client.subscribe("/d7/48363837002a0038/#")
