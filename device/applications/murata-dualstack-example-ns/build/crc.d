0b6e0f073cae550642bdac49840d7984096814fb
Jolanta Tadla <jolatadla3@tlen.pl>
2019-11-05 14:35:44 +0100

    deleting build

--- device/applications/murata-dualstack-example-ns/build/crc.d
+++
@@ -1,5 +0,0 @@
-../../applications/murata-dualstack-example-ns/build/crc.o: \
- ../../shields/Murata-dualstack/src/crc.c \
- ../../shields/Murata-dualstack/inc/crc.h
-
-../../shields/Murata-dualstack/inc/crc.h:
--- device/applications/murata-dualstack-example-ns/build/startup_stm32l496xx.d
+++
@@ -1,2 +0,0 @@
-../../applications/murata-dualstack-example-ns/build/startup_stm32l496xx.o: \
- ../../core/platform/octa/startup_stm32l496xx.s
