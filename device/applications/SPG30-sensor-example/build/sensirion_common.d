363e0d0966124d1eb8f4aad2c98eb3b31a8dc0eb
RubenJoosen <38206670+RubenJoosen@users.noreply.github.com>
2019-11-05 14:46:45 +0100

    ruben added

--- device/applications/SPG30-sensor-example/build/sensirion_common.d
+++
@@ -1,8 +0,0 @@
-../../applications/SPG30-sensor-example/build/sensirion_common.o: \
- ../../applications/SPG30-sensor-example/src/sensirion_common.c \
- ../../applications/SPG30-sensor-example/inc/sensirion_common.h \
- ../../applications/SPG30-sensor-example/inc/sensirion_configuration.h
-
-../../applications/SPG30-sensor-example/inc/sensirion_common.h:
-
-../../applications/SPG30-sensor-example/inc/sensirion_configuration.h:
--- device/applications/SPG30-sensor-example/build/startup_stm32l496xx.d
+++
@@ -1,2 +0,0 @@
-../../applications/SPG30-sensor-example/build/startup_stm32l496xx.o: \
- ../../core/platform/octa/startup_stm32l496xx.s
