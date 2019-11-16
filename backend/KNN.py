ce295bd827869f21fe0c426717ed9cad70c9926b
Student <student@student-02.idlab.uantwerpen.be>
2019-11-16 21:50:42 +0100

    database repaired to lodable state

--- backend/KNN.py
+++ backend/KNN.py
@@ -1,22 +1,39 @@
 import numpy as np
-from matplotlib import pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
+import json
 
-neigh = KNeighborsClassifier(n_neighbors=3, metric='euclidean',n_jobs=2)
-A = []
-B = []
-for i in range(1,10):
-    for j in range(1,5):
-        B.append(j+i)
-    A.append(B)
-    B=[]
-pos=[]
-for i in range(1,10):
-    for j in range(1,3):
-        B.append(j+i)
-    pos.append(B)
-    B=[]
-neigh.fit(A, pos)
-print(neigh.predict([[2.1,3.1, 3.9, 5]]))
-#print(neigh.predict_proba([[2.1, 3.1, 3.9, 5]]))
-plt.show()
+with open ("database2.repair.json","r") as readfile:
+    data = json.load(readfile)
+    print(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def knn():
+    neigh = KNeighborsClassifier(n_neighbors=3, metric='euclidean',n_jobs=2)
+    A = []
+    B = []
+    for i in range(1,10):
+        for j in range(1,5):
+            B.append(j+i)
+        A.append(B)
+        B=[]
+    pos=[]
+    for i in range(1,10):
+        for j in range(1,3):
+            B.append(j+i)
+        pos.append(B)
+        B=[]
+    neigh.fit(A, pos)
+    print(neigh.predict([[2.1,3.1, 3.9, 5]]))
+    #print(neigh.predict_proba([[2.1, 3.1, 3.9, 5]]))
