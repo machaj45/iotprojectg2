1622c577f8542d6cc10c7d245578587f3a269898
machaj45 <machaj45@fel.cvut.cz>
2019-11-17 15:59:47 +0100

    knn is now ploting to plot position of device based on signal strength

--- backend/learning/KNN.py
+++ backend/learning/KNN.py
@@ -1,5 +1,7 @@
 import numpy as np 
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import RadiusNeighborsRegressor
+from sklearn.neighbors import KNeighborsRegressor
 import json
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -22,12 +24,14 @@ for message in data.get("messages"):
         print(B)
         B=[]
 print(i)
-neigh = KNeighborsClassifier(n_neighbors=3, metric='euclidean',n_jobs=2)
-pos=[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+neigh = KNeighborsRegressor(n_neighbors=3,radius=1.5, metric='euclidean',n_jobs=2)
+pos=[[285,252],[285,252],[285,252],[285,326],[285,326],[285,326],[408,326],[408,326],[408,326],[408,252],[408,252],[408,252],[534,326],[534,326],[534,326],[534,252],[534,252],[671,326],[671,326],[671,326],[671,252],[671,252],[671,252]]
 neigh.fit(A, pos)
-print(neigh.predict([[53,50, 43, 38]]))
-print(neigh.predict_proba([[53,50, 43, 38]]))
-
+point = neigh.predict([[53,50, 43, 38]])[0]
+print(point)
+#print(neigh.predict_proba([[53,50, 43, 38]]))
+#print((neigh.kneighbors([[53,50, 43, 38]], n_neighbors=3, return_distance=False)))
 img=mpimg.imread('map.png')
 imgplot = plt.imshow(img)
+plt.plot(point[0],point[1],'r*')
 plt.show()
