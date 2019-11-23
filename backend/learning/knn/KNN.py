05d0f7b26e0e5787b589c63e02d77e1802d5a633
machaj45 <machaj45@fel.cvut.cz>
2019-11-23 19:14:59 +0100

    everythin should be repaired

--- backend/learning/knn/KNN.py
+++ backend/learning/knn/KNN.py
@@ -20,28 +20,8 @@ class KNN:
             data = json.load(readfile)
         A = []
         for message in data.get("messages"):
-<<<<<<< HEAD
-            for gateway in message:
-                i=i+1;
-                for gateways in message[gateway]:
-                    if gateways.get("gateway") != None:
-                        B.append(gateways.get("rxLevel"))
-                        C.append(gateways.get("gateway"))
-                B = [x for _,x in sorted(zip(C,B))] 
-                A.append(B)
-                print(B)
-                B=[]
-        #print(i)
-        self.neigh = KNeighborsRegressor(n_neighbors=6,radius=0.2, metric='euclidean',n_jobs=2)
-=======
             A.append(message)
-
-<<<<<<< HEAD
-        #self.neigh = KNeighborsRegressor(n_neighbors=3,radius=1.5, metric='euclidean',n_jobs=2)
->>>>>>> ebc25bed65c8879cfa9886642a36caf15ede6a77
-=======
         self.neigh = KNeighborsRegressor(n_neighbors=20,radius=10, metric='euclidean',n_jobs=2)
->>>>>>> b96219c5f2611b3ad5a469201452583877bed67a
 
         with open (filename2,"r") as readfile:
             data = json.load(readfile)
--- backend/learning/knn/how_to_use_knn.py
+++ backend/learning/knn/how_to_use_knn.py
@@ -2,18 +2,10 @@ from KNN import *
 import statistics
 
 kn = KNN()
-<<<<<<< HEAD
-<<<<<<< HEAD
 signal = [[53,46, 55, 39]]
 point = kn.getpoint(signal)
 print(point)
-=======
-signal = [[53,50, 43, 38]]
-#point = kn.getpoint(signal)
-#print(point)
 point=[0,0]
->>>>>>> ebc25bed65c8879cfa9886642a36caf15ede6a77
-=======
 #signal = [[45,42,55,34],[54,30,59,34],[52,38,55,32],[41,45,42,27],[41,41,52,39]]
 signal = [[58,45,53,44], [52,42,42,31], [52,54,44,34], [64,60,50,36]]
 A = []
@@ -30,11 +22,10 @@ X.append(statistics.mean(A))
 X.append(statistics.mean(B))
 X.append(statistics.mean(C))
 X.append(statistics.mean(D))
-#print(D)
+
 A = []
 A.append(X)
 point = kn.getpoint(A)
 print(point)
->>>>>>> b96219c5f2611b3ad5a469201452583877bed67a
 kn.plotmap(point)
 
